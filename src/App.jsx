f5fa632284c714f3e72a5300fd9409844804f488
Maxim Nikolus <nikolusmaxim409@gmail.com>
2024-12-17 21:53:19 +0100

    add react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Lesson01 from "./lessons/lesson01/Lesson01";
+
+function App() {
+  return (
+    <>
+      <Lesson01></Lesson01>
+    </>
+  );
+}
+
+export default App;
---
+++ src/lessons/lesson01/Lesson01.jsx
@@ -0,0 +1,28 @@
+import viteLogo from "/vite.svg";
+import reactLogo from "../../assets/react.svg";
+
+function Lesson01() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1> Hello React</h1>
+
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default Lesson01;
